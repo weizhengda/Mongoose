241bb92537bd01ceff3a3f923305c9f22ff39b78
weizhengda <2625785357@qq.com>
2017-05-05 21:39:40 +0800

    环境良好

---
+++ code/part1/model.js
@@ -0,0 +1,11 @@
+#mongoose
+
+##not
+
+* [part1:Model&Schema](not/part1.md)
+* [part2:Express&Mongoose](not/part2.md)
+* [part3:模式的扩展](not/part3.md)
+* [part4:模型的方法](not/part4.md)
+* [part5:数据的校验](not/part5.md)
+* [part6:中间件](not/part6.md)
+* [part7:DBRef](not/part7.md)
